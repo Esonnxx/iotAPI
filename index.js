188ade21b682ea6b4f2116fe957e1883915d4256
Esonnxx <77544102+Esonnxx@users.noreply.github.com>
2023-06-03 20:32:36 +0800

    Update index.js
    sec

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express()
 const port = 3000
 
 const dbConfig = {
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  host: "database-1.cmvjkstmwutb.ap-southeast-2.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "mypassword",
+  database: "iotData"
 }
 const db = mysql.createConnection(dbConfig)
 db.connect((error) => {
